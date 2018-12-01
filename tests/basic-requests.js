57a76527e67f84bf6850a69d4314b8f4a30c17c3
percho <ivanhuay@gmail.com>
2018-11-30 23:11:41 -0300

    params test working fine

--- tests/basic-requests.js
+++ tests/basic-requests.js
@@ -60,5 +60,77 @@ describe('basic request', () => {
                     });
             });
         });
+        describe('multiple preloaded data & params', () => {
+            before(() => {
+                return Promise.all([
+                    Users.create({
+                        _id: '5c01997482c8985ad9a7eb5b',
+                        name:'test user',
+                        email: 'test@lts.com'
+                    }),
+                    Users.create({
+                        _id: '5c01997482c8985ad9a7eb5c',
+                        name:'mario bross',
+                        email: 'aaaa@lts.com'
+                    }),
+                    Users.create({
+                        _id: '5c01997482c8985ad9a7eb5d',
+                        name:'lex',
+                        email: 'main.lex@lts.com'
+                    })
+                ]);
+            });
+            after(() => {
+                return Users.deleteMany({});
+            });
+            it('should get 3 user', () => {
+                return request(app)
+                    .get('/api/users')
+                    .set('Accept', 'application/json')
+                    .expect(200)
+                    .then((response) => {
+                        assert.equal(response.body.length, 3);
+                        assert.equal(response.body[0].name, 'test user');
+                    });
+            });
+            it('should return count user', () => {
+                return request(app)
+                    .get('/api/users?count=true')
+                    .set('Accept', 'application/json')
+                    .expect(200)
+                    .then((response) => {
+                        assert.equal(response.body, 3);
+                    });
+            });
+            it('should return 2 user with limit', () => {
+                return request(app)
+                    .get('/api/users?limit=2')
+                    .set('Accept', 'application/json')
+                    .expect(200)
+                    .then((response) => {
+                        assert.equal(response.body.length, 2);
+                    });
+            });
+            it('should return users sorted by email', () => {
+                return request(app)
+                    .get('/api/users?sort=email')
+                    .set('Accept', 'application/json')
+                    .expect(200)
+                    .then((response) => {
+                        assert.equal(response.body.length, 3);
+                        assert.equal(response.body[0].email, 'aaaa@lts.com');
+                    });
+            });
+            it('should return skyp first user sorted by email', () => {
+                return request(app)
+                    .get('/api/users?sort=email&skip=1')
+                    .set('Accept', 'application/json')
+                    .expect(200)
+                    .then((response) => {
+                        assert.equal(response.body.length, 2);
+                        assert.equal(response.body[0].email, 'main.lex@lts.com');
+                    });
+            });
+        });
     });
 });
