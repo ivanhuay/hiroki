260d42f825529f13474296743f994727442b2b60
percho <ivanhuay@gmail.com>
2018-12-06 08:06:25 -0300

    add travis

--- tests/mock/app.js
+++ tests/mock/app.js
@@ -4,7 +4,7 @@ const buildHiroki = require('./build-hiroki');
 const mongoose = require('mongoose');
 const app = express();
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/test', {useNewUrlParser: true});
+mongoose.connect('mongodb://travis:test@localhost:27017/test', {useNewUrlParser: true});
 app.use(buildHiroki());
 
 module.exports = app;
